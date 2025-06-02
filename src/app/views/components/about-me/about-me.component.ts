7d7dc3aa7c4fe83afce6539436553fafc214372d
2014maximo <alexuriel.777@gmail.com>
2025-06-02 09:02:37 -0500

    Modal about-me in construction

--- src/app/views/components/about-me/about-me.component.ts
+++ src/app/views/components/about-me/about-me.component.ts
@@ -1,4 +1,6 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-about-me',
--- src/app/views/components/skills/skills.component.ts
+++ src/app/views/components/skills/skills.component.ts
@@ -1,9 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ModuleWithProviders } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgCircleProgressModule],
+  providers: [
+    (NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: '#78C000',
+      innerStrokeColor: '#C7E596',
+      animationDuration: 300,
+    }) as ModuleWithProviders<NgCircleProgressModule>).providers!,
+  ],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
