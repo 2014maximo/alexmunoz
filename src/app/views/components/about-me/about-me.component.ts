d07b18dce8c626489c05162c520e8a359191ba16
auriel1 <auriel@smartsoftlabs.com>
2025-06-08 22:19:03 -0500

    Ajustment form contact

--- src/app/views/components/about-me/about-me.component.ts
+++ src/app/views/components/about-me/about-me.component.ts
@@ -1,14 +1,33 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import * as AOS from 'aos';
+import { FormContactComponent } from '../form-contact/form-contact.component';
 
 @Component({
-  selector: 'app-about-me',
-  standalone: true,
-  imports: [],
-  templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+	selector: 'app-about-me',
+	standalone: true,
+	imports: [ReactiveFormsModule, FormContactComponent],
+	templateUrl: './about-me.component.html',
+	styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
+	contactForm: FormGroup;
 
+	constructor(private fb: FormBuilder) {
+		this.contactForm = this.fb.group({
+			email: ['', [Validators.required, Validators.email]],
+			comments: ['', [Validators.required, Validators.minLength(5)]]
+		});
+	}
+	ngOnInit(): void {
+		AOS.init();
+	  }
+	
+	  onSubmit() {
+		if (this.contactForm.valid) {
+		  // Aquí puedes manejar el envío, por ejemplo mostrar un mensaje o enviar a un backend
+		  alert('Formulario enviado: ' + JSON.stringify(this.contactForm.value));
+		  this.contactForm.reset();
+		}
+	  }
 }
--- src/app/views/components/form-contact/form-contact.component.ts
+++ src/app/views/components/form-contact/form-contact.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './form-contact.component.html',
   styleUrl: './form-contact.component.scss'
 })
 export class FormContactComponent {
+contactForm: FormGroup;
+
+constructor(private fb: FormBuilder) {
+		this.contactForm = this.fb.group({
+			email: ['', [Validators.required, Validators.email]],
+			comments: ['', [Validators.required, Validators.minLength(5)]]
+		});
+	}
+
+  onSubmit() {
+		if (this.contactForm.valid) {
+		  // Aquí puedes manejar el envío, por ejemplo mostrar un mensaje o enviar a un backend
+		  alert('Formulario enviado: ' + JSON.stringify(this.contactForm.value));
+		  this.contactForm.reset();
+		}
+	  }
 
 }
