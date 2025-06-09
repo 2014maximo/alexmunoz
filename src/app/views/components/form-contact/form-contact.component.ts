a50175a6fd77bbfadd1da221e79c38b71848fb86
auriel1 <auriel@smartsoftlabs.com>
2025-06-08 22:22:55 -0500

    add Common Module in Form contact component

--- src/app/views/components/form-contact/form-contact.component.ts
+++ src/app/views/components/form-contact/form-contact.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-contact',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-contact.component.html',
   styleUrl: './form-contact.component.scss'
 })
