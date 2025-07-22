7ff19df6695c25c40b540616d5197d9044e78771
2014maximo <alexuriel.777@gmail.com>
2025-07-21 19:55:00 -0500

    finish responsive 1

--- src/app/views/components/projects/constants/project.constants.ts
+++ src/app/views/components/projects/constants/project.constants.ts
@@ -10,9 +10,9 @@ export const PROJECTS: IProject[] = [
         imageUrl: 'assets/img/projects/ecommerce-dashboard.png',
         features: [
             'auth',
-            'Gestión de inventario en tiempo real',
-            'Análisis de ventas con gráficos',
-            'Panel de administración responsive'
+            'invReal',
+            'aVentas',
+            'pResponsive'
         ]
     },
     {
@@ -23,24 +23,24 @@ export const PROJECTS: IProject[] = [
         imageUrl: '	https://2014maximo.github.io/workface/assets/img/icons/logo-workface-white.png',
         demoUrl: 'https://2014maximo.github.io/workface/home',
         features: [
-            'Autenticación con Firebase Auth',
-            'Encriptación de datos con Firestore',
-            'Generación de imágenes de perfil con html-to-image',
-            'Interfaz de usuario responsiva con Angular bootstrap'
+            'aFiresAuth',
+            'eFirestore',
+            'gImagenes',
+            'inAngular'
         ]
     },
     {
         title: 'PathPilot',
-        description: 'Aplicación web para solucionar el problema del viajante, optimización de grupo de paradas para un viaje en las distancias más cortas.',
+        description: 'dPathPilot',
         technologies: ['Angular', 'Github pages', 'API de Google Maps'],
         githubUrl: 'https://github.com/tuUsuario/weather-pwa',
         demoUrl: 'https://weather-pwa-demo.com',
         imageUrl: 'assets/img/projects/weather-app.png',
         features: [
-            'Autenticación con Firebase Auth',
-            'Interfaz de usuario intuitiva',
-            'Integración con la API de Google Maps para optimizar rutas',
-            'Visualización de rutas en un mapa interactivo'
+            'aFiresAuth',
+            'interIntui',
+            'apiGoogle',
+            'VRutas'
         ]
     }
 ];
