5fd10f50d52c0282ed33de4cb59a3cb71b6b764a
2014maximo <alexuriel.777@gmail.com>
2025-07-24 16:16:32 -0500

    file translate

--- src/app/views/components/about-me/about-me.component.ts
+++ src/app/views/components/about-me/about-me.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import * as AOS from 'aos';
 import { FormContactComponent } from '../form-contact/form-contact.component';
 
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateModule, TranslateService } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -15,11 +16,40 @@ import { CommonModule } from '@angular/common';
 })
 export class AboutMeComponent {
 
+	espanish: boolean = true;
+	private langChangeSubscription: Subscription | null = null;
+
 	constructor(public translate: TranslateService,) {
 		this.translate.setDefaultLang('es');
 	}
 
 	ngOnInit(): void {
 		AOS.init();
-	  }
+		 // Suscríbete al evento onLangChange
+		 this.langChangeSubscription = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+			console.log('El idioma ha cambiado a:', event.lang);
+			// Aquí puedes actualizar tu variable booleana
+			this.espanish = true; // O false, dependiendo de tu lógica
+		  });
+	  
+		  // Opcional: Para establecer el estado inicial de la variable
+		  // Puedes verificar el idioma actual al inicio si lo necesitas
+		  if (this.translate.currentLang !== this.translate.defaultLang) {
+			this.espanish = true;
+		  } else {
+			this.espanish = false;
+		  }
+	}
+
+	ngOnDestroy(): void {
+		// Asegúrate de desuscribirte para evitar fugas de memoria
+		if (this.langChangeSubscription) {
+		  this.langChangeSubscription.unsubscribe();
+		}
+	}
+
+	changeDownload(){
+		this.espanish = !this.espanish;
+	}
+
 }
