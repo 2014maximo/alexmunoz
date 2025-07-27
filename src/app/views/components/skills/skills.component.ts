195991f8e55797ab9cfa89a34052dd8d26f399ef
auriel1 <auriel@smartsoftlabs.com>
2025-07-26 22:48:29 -0500

    adjust ending version 2

--- src/app/views/components/skills/skills.component.ts
+++ src/app/views/components/skills/skills.component.ts
@@ -1,32 +1,37 @@
 import { Component, ModuleWithProviders } from '@angular/core';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import * as AOS from 'aos';
 
 @Component({
-  selector: 'app-skills',
-  standalone: true,
-  imports: [NgCircleProgressModule,  TranslateModule],
-  providers: [
-    (NgCircleProgressModule.forRoot({
-      radius: 100,
-      outerStrokeWidth: 16,
-      innerStrokeWidth: 8,
-      outerStrokeColor: '#78C000',
-      innerStrokeColor: '#C7E596',
-      animationDuration: 300,
-    }) as ModuleWithProviders<NgCircleProgressModule>).providers!,
-  ],
-  templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+	selector: 'app-skills',
+	standalone: true,
+	imports: [NgCircleProgressModule, TranslateModule],
+	providers: [
+		(NgCircleProgressModule.forRoot({
+			radius: 100,
+			outerStrokeWidth: 16,
+			innerStrokeWidth: 8,
+			outerStrokeColor: '#78C000',
+			innerStrokeColor: '#C7E596',
+			animationDuration: 300,
+		}) as ModuleWithProviders<NgCircleProgressModule>).providers!,
+	],
+	templateUrl: './skills.component.html',
+	styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
 
-  active: boolean = true;
+	active: boolean = true;
 
-  constructor(public translate: TranslateService,) {
-    this.translate.addLangs(['fr', 'en', 'es']);
-	    this.translate.setDefaultLang('es');
-	    this.translate.use('es');
-  }
+	constructor(public translate: TranslateService,) {
+		this.translate.addLangs(['fr', 'en', 'es']);
+		this.translate.setDefaultLang('es');
+		this.translate.use('es');
+	}
+
+	ngOnInit(): void {
+		AOS.init();
+	}
 
 }
